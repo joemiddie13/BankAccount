7cd3d6fcb5ed0035308decaae78ce4b4b83d3b60
joemiddie13 <joemiddie13@gmail.com>
2023-10-23 14:53:09 -0700

    added an account number generator

--- BankAccount.py
+++ BankAccount.py
@@ -1,5 +1,13 @@
+import random
+
 class BankAccount:
-  def __init__(self, full_name, account_number, balance):
-    self.name = full_name
-    self.account = account_number
-    self.balance = balance
+  def __init__(self, full_name,): #account_number, #balance):
+    self.full_name = full_name
+    self.account_number = self.generate_account_number()
+    self.balance = 0
+  
+  def generate_account_number(self):
+    return random.randint(10000000, 99999999)
+
+rocko_bank = BankAccount("Rocko Paul")
+print(rocko_bank.account_number)
