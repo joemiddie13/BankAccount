2ffcbe3dc976ab821e8a068ea48d5a8d926f70cf
joemiddie13 <joemiddie13@gmail.com>
2023-10-23 17:08:42 -0700

    edited a couple of methods, example 6 Mitchell

--- BankAccount.py
+++ BankAccount.py
@@ -1,9 +1,12 @@
 import random
 
 class BankAccount:
-  def __init__(self, full_name,): #account_number, #balance):
+  def __init__(self, full_name, account_number=None):
     self.full_name = full_name
-    self.account_number = self.generate_account_number()
+    if account_number:
+      self.account_number = account_number
+    else:
+      self.account_number = self.generate_account_number()
     self.balance = 0
   
   def generate_account_number(self):
@@ -26,22 +29,58 @@ class BankAccount:
     return self.balance
   
   def add_interest(self):
-    interest = []
     annual_interest = 0.00083
     interest = self.balance * annual_interest
-    return interest
+    self.balance += interest
 
-rocko_bank = BankAccount("Rocko Paul")
+  def print_statement(self):
+    formatted_balance = "{:,.2f}".format(self.balance)
+    print(f"""
+          {self.full_name}
+          Account No.: {self.account_number}
+          Balance: ${formatted_balance}
+          """)
 
-print(f"Initial balance: ${rocko_bank.balance:.2f}")
+# (5) Rocko Example
 
-rocko_bank.deposit(1000)
-print(f"After deposit: {rocko_bank.balance:.2f}")
+# rocko_bank = BankAccount("Rocko Paul")
 
-rocko_bank.withdraw(500)
-print(f"After withdraw: {rocko_bank.balance:.2f}")
+# print(f"Initial balance: ${rocko_bank.balance:.2f}")
 
-rocko_bank.get_balance()
+# rocko_bank.deposit(1000)
+# print(f"After deposit: {rocko_bank.balance:.2f}")
 
-rocko_bank.add_interest()
-rocko_bank.get_balance()
+# rocko_bank.withdraw(500)
+# print(f"After withdraw: {rocko_bank.balance:.2f}")
+
+# rocko_bank.get_balance()
+
+# rocko_bank.add_interest()
+# rocko_bank.get_balance()
+
+# rocko_bank.print_statement()
+
+#########################################################################
+
+# (6) Mitchell Example!
+
+# Instantiate Mitchell's bank account information using BankAccount class
+mitchell_bank = BankAccount("Mitchell Hudson", "03141592")
+
+# Initialize a deposit of $400,000 to Mitchell's bank account
+mitchell_bank.deposit(400000)
+
+# Print Mitchell's bank account statement
+mitchell_bank.print_statement()
+
+# Add interest to Mitchell's bank account
+mitchell_bank.add_interest()
+
+# Print Mitchell's updated bank account statement after interest
+mitchell_bank.print_statement()
+
+# Withdraw $150 so Mitchell can fit some fresh new Nike's
+mitchell_bank.withdraw(150)
+
+# Print final statement of Mitchell's spending habit for the day
+mitchell_bank.print_statement()
